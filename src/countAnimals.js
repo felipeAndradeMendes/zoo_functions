16ac57d0ab7de91847891962ed9c70bf6390f5ec
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 18:33:08 -0300

    Conclui parte 1 do requisito 05 - pausa no dia, continuo amanhã

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  const totalResidentsArr = species.reduce((acc, cur) => {
+    const { name, residents } = cur;
+
+    acc[name] = residents.length;
+    return acc;
+  }, {});
+  return totalResidentsArr;
 };
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
