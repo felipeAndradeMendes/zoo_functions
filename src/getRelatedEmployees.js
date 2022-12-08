e1b647ac2f6c0dc554acae5da6699583a72a35ea
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 16:30:57 -0300

    Conclui primeira função do requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 const isManager = (id) => {
-  // seu código aqui
+  const managers = employees.map((employee) => employee.managers);
+  const newArr = [...new Set(managers.flat())];
+  return newArr.includes(id);
 };
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
