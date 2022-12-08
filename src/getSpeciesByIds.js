c5c7843cc34e2fb3f2a2d0de1165f8c8d7447bb2
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 15:12:52 -0300

    Conclui requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const { species } = data;
+
+const getSpeciesByIds = (...ids) => {
+  const idsArr = ids;
+  return species.filter((specie) => idsArr.some((animal) => animal === specie.id));
 };
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
 
 module.exports = getSpeciesByIds;
