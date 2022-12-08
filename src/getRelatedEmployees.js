da647e66da6fa2ecdac86bb38faa70c964268544
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 17:30:56 -0300

    Requisito 04 incabado. falta corrigir um erro de jest

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,12 +5,28 @@ const { employees } = data;
 const isManager = (id) => {
   const managers = employees.map((employee) => employee.managers);
   const newArr = [...new Set(managers.flat())];
+
   return newArr.includes(id);
 };
 // console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  try {
+    if (isManager(managerId) === false) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+  } catch (error) {
+    // console.log(typeof error.message)
+    return error.message;
+  }
+
+  const employeesManaged = employees.filter((employee) => employee.managers.includes(managerId));
+  const employeesName = employeesManaged.map((emp) => `${emp.firstName} ${emp.lastName}`);
+  // console.log('bunda velhas')
+  return employeesName;
 };
+
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
