a67b806c328e613e22ca5ec2c3abf7ce410aa347
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 15:29:53 -0300

    Conclui requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalFound = species.find((specie) => specie.name === animal);
+  // console.log(animalFound.residents);
+  const agesMap = animalFound.residents.map((resident) => resident.age);
+  const minAge = agesMap.every((animalAge) => animalAge > age);
+
+  return minAge;
 };
+
+// console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
