e7664934840d061a886c1583e04169a9bc041c9a
Felipe Mendes <felipemadruga@gmail.com>
2022-12-08 15:56:35 -0300

    Conclui requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+
+  const employeeFound = employees.find((employee) => {
+    const { firstName, lastName } = employee;
+    return firstName === employeeName || lastName === employeeName;
+  });
+
+  return employeeFound;
 };
+
+// console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
