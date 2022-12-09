ce27e5a43ac04542283decb7d8b834d2a1d06564
Felipe Mendes <felipemadruga@gmail.com>
2022-12-09 16:33:42 -0300

    Conclui requisito 05, porem falta um erro de lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,15 +3,39 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 const countAnimals = (animal) => {
-  const totalResidentsArr = species.reduce((acc, cur) => {
-    const { name, residents } = cur;
+  if (animal === undefined) {
+    const totalResidentsArr = species.reduce((acc, cur) => {
+      const { name, residents } = cur;
 
-    acc[name] = residents.length;
-    return acc;
-  }, {});
-  return totalResidentsArr;
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+    return totalResidentsArr;
+  }
+
+  const animalsFound = species.find((specie) => {
+    const { name } = specie;
+    return name === animal.species;
+  });
+
+  const { residents } = animalsFound;
+  if (animal.sex === undefined) {
+    return residents.length;
+  }
+
+  const chosenResidents = residents.reduce((acc, cur) => {
+    let sum = 0;
+    if (cur.sex === animal.sex) { sum = 1; }
+    return acc + sum;
+  }, 0);
+  return chosenResidents;
 };
 
-console.log(countAnimals());
+console.log(countAnimals({ species: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
+
+// console.log('Nome da especie', name);
+// console.log('Residentes', residents);
+// console.log('Parametro da especie', species);
+// console.log('Parametro do sexo', sex);
