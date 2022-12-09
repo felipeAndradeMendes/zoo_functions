4d9d42316e22d308e5eaa85555e17300bf4a5d17
Felipe Mendes <felipemadruga@gmail.com>
2022-12-09 16:50:58 -0300

    Conclui requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,21 +2,27 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const countAnimals = (animal) => {
-  if (animal === undefined) {
-    const totalResidentsArr = species.reduce((acc, cur) => {
-      const { name, residents } = cur;
-
-      acc[name] = residents.length;
-      return acc;
-    }, {});
-    return totalResidentsArr;
-  }
-
-  const animalsFound = species.find((specie) => {
+const findAnimal = (animal) => {
+  const animalFound = species.find((specie) => {
     const { name } = specie;
     return name === animal.species;
   });
+
+  return animalFound;
+};
+
+const noParameter = () => {
+  const totalResidentsArr = species.reduce((acc, cur) => {
+    const { name, residents } = cur;
+    acc[name] = residents.length;
+    return acc;
+  }, {});
+  return totalResidentsArr;
+};
+
+const countAnimals = (animal) => {
+  if (animal === undefined) return noParameter();
+  const animalsFound = findAnimal(animal);
 
   const { residents } = animalsFound;
   if (animal.sex === undefined) {
@@ -28,6 +34,7 @@ const countAnimals = (animal) => {
     if (cur.sex === animal.sex) { sum = 1; }
     return acc + sum;
   }, 0);
+
   return chosenResidents;
 };
 
@@ -39,3 +46,18 @@ module.exports = countAnimals;
 // console.log('Residentes', residents);
 // console.log('Parametro da especie', species);
 // console.log('Parametro do sexo', sex);
+
+// findAnimal(animal)
+// const animalsFound = species.find((specie) => {
+//   const { name } = specie;
+//   return name === animal.species;
+// });
+
+// if (animal === undefined) {
+//   const totalResidentsArr = species.reduce((acc, cur) => {
+//     const { name, residents } = cur;
+//     acc[name] = residents.length;
+//     return acc;
+//   }, {});
+//   return totalResidentsArr;
+// }
