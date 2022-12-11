8fb711de8ae8ae9ecd45d5ba24028dbcbbcd743e
Felipe Mendes <felipemadruga@gmail.com>
2022-12-11 20:33:33 -0300

    Conclui requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,12 +2,16 @@ const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
+const employeesIds = employees.map((employee) => employee.id);
+// console.log('EMPLOYEES IDS:', employeesIds)
+
 const getLocation = (info) => {
   const speciesFound = info.map((id) => species.find((specie) => specie.id === id).location);
   return speciesFound;
 };
 
 const getEmployee = (param) => {
+
   if (param.name) {
     return employees.find((employee) => {
       const { firstName, lastName } = employee;
@@ -19,14 +23,13 @@ const getEmployee = (param) => {
   if (param.id) {
     const employeeId = employees.find((employee) => employee.id === param.id);
     // console.log('RETORNO DO FIND NO ID', employeeId);
-    if (employeeId === undefined) {
-      throw new Error('Informações inválidas');
-    }
+    // if (employeeId === undefined) {
+    //   throw new Error('Informações inválidas');
+    // }
     return employeeId;
   }
 };
 
-// console.log('TESTE GEEMPLOYEE', getEmployee({ id: 'Id inválido' }));
 
 const changeIdForName = (ids) => {
   const idsFound = ids.map((id) => species.filter((specie) => specie.id === id));
@@ -48,6 +51,16 @@ const getFullEmployeeList = () => {
   return formatList;
 };
 
+const validatedId = (param) => {
+  const id = param.id;
+  if (param.id) {
+    if(!employeesIds.includes(id)) {
+      throw new Error('Informações inválidas');
+    }
+  }
+  return null;
+}
+
 // if(getEmployee(objParam) === undefined) {
 //   console.log('OBJCOM PARAMETRO', getEmployee(objParam))
 //   throw new Error('Informações inválidas')
@@ -55,29 +68,31 @@ const getFullEmployeeList = () => {
 // }
 
 const getEmployeesCoverage = (objParam) => {
+  if (objParam !== undefined) validatedId(objParam);
+
   if (objParam) {
-    try {
-      getEmployee(objParam);
-    } catch (error) {
-      return error.message;
-    }
-    const employeeFound = getEmployee(objParam);
-    const { id, firstName, lastName, responsibleFor } = employeeFound;
-    const employeeFoundFormated = {
-      id,
-      fullName: `${firstName} ${lastName}`,
-      species: changeIdForName(responsibleFor),
-      locations: getLocation(responsibleFor),
-    };
+    // try {
+      //   getEmployee(objParam);
+      // } catch (error) {
+        //   return error.message;
+        // }
+        const employeeFound = getEmployee(objParam);
+        const { id, firstName, lastName, responsibleFor } = employeeFound;
+        const employeeFoundFormated = {
+          id,
+          fullName: `${firstName} ${lastName}`,
+          species: changeIdForName(responsibleFor),
+          locations: getLocation(responsibleFor),
+        };
     return employeeFoundFormated;
   }
   const fullEmployeeList = getFullEmployeeList();
   return fullEmployeeList;
 };
 
-// console.log(getEmployeesCoverage({ name: 'Spry' }))
-console.log(getEmployeesCoverage({ id: 'Id inválido' }));
+// console.log(getEmployeesCoverage({ name: 'Spry' }));
+// console.log(getEmployeesCoverage({ id: 'Id QUALQUER' }));
 // console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }))
-// console.log(getEmployeesCoverage());
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
