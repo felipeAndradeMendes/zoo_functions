682217b82777c69d4cb1226e5271388f54b0cebc
Felipe Mendes <felipemadruga@gmail.com>
2022-12-11 19:35:54 -0300

    Agora sim, conclui requisito 12 - o test coverage muda a abrangencia de acordo com novos testes.

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,4 +24,24 @@ describe('Testes da função getOpeningHours', () => {
   it('Se a abreviação for diferente de "am" ou "pm" retorna o erro esperado', () => {
     expect(() => getOpeningHours('sunday', '08:50-pp')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
+
+  it('Se hora for menor que 0 ou maior que 12, retorna o erro esperado', () => {
+    expect(() => getOpeningHours('tuesday', '13:20-am')).toThrow('The hour must be between 0 and 12');    
+  });
+
+  it('Se o minuto for menor que 0 ou maior que 59, retorna o erro esperado', () => {
+    expect(() => getOpeningHours('tuesday', '10:60-am')).toThrow('The minutes must be between 0 and 59');
+  });
+
+  it('Deve retornar o erro correto quando o parametro dia não fizer parte da semana', () => {
+    expect(() => getOpeningHours('jumaday', '11:35-pm')).toThrow('The day must be valid. Example: Monday');
+  });
+
+  it('Deve retornar que o Zoo está fechado quando o dia for "monday"', () => {
+    expect(getOpeningHours('monday', '09:55-am')).toBe('The zoo is closed');
+  });
+
+  it('Retorna que o Zoo está aberto num dia especifico', () => {
+    expect(getOpeningHours('Wednesday', '09:15-am')).toBe('The zoo is open');
+  });
 });
