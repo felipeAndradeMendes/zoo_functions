f8054a04ee61efad306577e4fc00ee836be6d71d
Felipe Mendes <felipemadruga@gmail.com>
2022-12-11 09:52:03 -0300

    Requisito 08 parcialmente feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,33 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const entrants = [
+	{ name:  'Lara Carvalho', age:  5 },
+	{ name:  'Frederico Moreira', age:  5 },
+	{ name:  'Pedro Henrique Carvalho', age:  5 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+];
 
-const calculateEntry = (entrants) => {
-  // seu código aqui
+const countEntrants = (entrants) => {
+  let sumChild = 0;
+  let sumAdult = 0;
+  let sumSenior = 0;
+  const res = entrants.forEach((cur) => {
+    if (cur.age < 18) sumChild += 1;  
+    if (cur.age >= 50)sumSenior += 1;    
+    if (cur.age >= 18 && cur.age < 50) sumAdult += 1;        
+  });
+  objRes = { child: sumChild, adult: sumAdult, senior: sumSenior };
+  return objRes
 };
+// console.log(countEntrants(entrants));
+
+const calculateEntry = (entrants = 0) => {
+  // if (entrants === undefined) return 0;
+  if (entrants = {}) return 0;
+};
+console.log(calculateEntry({}));
 
 module.exports = { calculateEntry, countEntrants };
 //
