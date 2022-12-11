6f0ba5966de89145d56aad8b2b017e180fdf1867
Felipe Mendes <felipemadruga@gmail.com>
2022-12-11 13:26:23 -0300

    Conclui requisito 09

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,57 +2,55 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
+const getFullList = (listOfDays) => {
+  const officeHourExibitionList = listOfDays.map((day) => {
+    const animalName = species.filter((specie) => specie.availability.includes(day))
+      .map((animal) => animal.name);
+
+    if (day === 'Monday') {
+      return ({
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      });
+    }
+
+    return ({
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: animalName,
+    });
+  });
+  return officeHourExibitionList;
+};
+
+const isAnimalName = (param, speciesList) => {
+  const targetSpecie = species.find((specie) => specie.name === param);
+  return targetSpecie.availability;
+};
+
 const getSchedule = (scheduleTarget) => {
   const speciesZoo = species.map((specie) => specie.name);
   const hourKeys = Object.keys(hours);
-  
+  const officeHourExibitionList = getFullList(hourKeys);
+  const finalList = officeHourExibitionList.reduce((acc, cur, index) => {
+    acc[hourKeys[index]] = cur;
+    return acc;
+  }, {});
+
   if (speciesZoo.includes(scheduleTarget)) {
-    const targetSpecie = species.find((specie) => specie.name === scheduleTarget);
-    return targetSpecie.availability;
+    return isAnimalName(scheduleTarget, speciesZoo);
   }
 
-  
-  const officeHourExibitionList = hourKeys.map((day) => {
-    const animalName = species.filter((specie) => specie.availability.includes(day)).map((animal) => animal.name);
-    
-    if (day === 'Monday') return ({
-      'officeHour': 'CLOSED',
-      'exhibition': 'The zoo will be closed!',
-    })   
-    
-    return ({
-      'officeHour': `Open from ${hours[day].open}am until ${hours[day].close}pm`,
-      'exhibition': animalName,
-    })    
-  });
-  // console.log('LISTA DE DIAS E HORARIOS:', officeHourExibitionList);
- 
-  
-  const finalList = officeHourExibitionList.reduce((acc, cur, index) => {
-    // console.log('DIA DA SEMANA', hourKeys[index]);
-    // console.log('ACC:', acc);
-    // console.log('CUR:', cur);
-    
-    acc[hourKeys[index]] = cur;
-    return acc
-  }, {})
-  
   if (hourKeys.includes(scheduleTarget)) {
     const daysAndHours = Object.entries(finalList);
     const dayFiltered = daysAndHours.filter((day) => day.includes(scheduleTarget));
     return Object.fromEntries(dayFiltered);
   }
-  
-  
   return finalList;
 };
 
-console.log(getSchedule('Mon'));
+// console.log(getSchedule());
 
 module.exports = getSchedule;
-
-
-const teste = species.filter((specie) => specie.availability.includes('Tuesday')).map((animal) => animal.name);
 
 // console.log(teste);
 // const obj = {};
@@ -63,3 +61,25 @@ const teste = species.filter((specie) => specie.availability.includes('Tuesday')
 // obj[hourKeys[1]] = officeHourExibitionList[0];
 
 // console.log('OBJETO', obj);
+
+// const officeHourExibitionList = hourKeys.map((day) => {
+//   const animalName = species.filter((specie) => specie.availability.includes(day))
+//     .map((animal) => animal.name);
+
+//   if (day === 'Monday') {
+//     return ({
+//       officeHour: 'CLOSED',
+//       exhibition: 'The zoo will be closed!',
+//     });
+//   }
+
+//   return ({
+//     officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+//     exhibition: animalName,
+//   });
+// });
+
+// if (speciesZoo.includes(scheduleTarget)) {
+//   const targetSpecie = species.find((specie) => specie.name === scheduleTarget);
+//   return targetSpecie.availability;
+// }
