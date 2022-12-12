195d6678293d0937b12cbca8b03e400f336ff615
Felipe Mendes <felipemadruga@gmail.com>
2022-12-12 13:36:00 -0300

    Arruma alguns erros de lint. Ainda faltam alguns

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -4,7 +4,7 @@ const { species } = data;
 
 const getLocation = (initials) => {
   const initFormated = initials.toUpperCase();
-  const speciesFound =  species.filter((specie) => specie.location === initFormated);
+  const speciesFound = species.filter((specie) => specie.location === initFormated);
   const speciesFoundNames = speciesFound.map((specie) => specie.name);
   return speciesFoundNames;
 };
@@ -24,34 +24,38 @@ const getAnimalsNames = (animal, animalSex) => {
   const specieFound = species.find((specie) => specie.name === animal);
   const { residents } = specieFound;
   let animalNames = residents.map((resident) => resident.name);
-  
+
   if (animalSex) {
-    animalNames = residents.filter((resident) => resident.sex === animalSex).map((residentName) => residentName.name);
+    animalNames = residents.filter((resident) => resident.sex === animalSex)
+      .map((residentName) => residentName.name);
     // console.log('RETORNO DENTRO DO IF', animalNames)
   }
   // console.log('RETORNO CO PRAMETRO DE SEX', animalNames)
 
   // sortNames(animalNames);
-  const objNames = {[animal]: animalNames};
+  const objNames = { [animal]: animalNames };
 
   return objNames;
 };
 
 const getListWithNames = (param1, paramSex) => {
   console.log('PARAM SEX NA FUNÇÃO SEM SORT', paramSex);
-  const allLocations =  getAllLocations();
+  const allLocations = getAllLocations();
   const { NE, NW, SE, SW } = allLocations;
   const animalNameListNE = NE.map((animal) => getAnimalsNames(animal, paramSex));
   const animalNameListNW = NW.map((animal) => getAnimalsNames(animal, paramSex));
   const animalNameListSE = SE.map((animal) => getAnimalsNames(animal, paramSex));
   const animalNameListSW = SW.map((animal) => getAnimalsNames(animal, paramSex));
+  const neLoc = 'NE';
+  const nwLoc = 'NW';
+  const seLoc = 'SE';
+  const swLoc = 'SW';
   const animalNameListOBJ = {
-    'NE': animalNameListNE,
-    'NW': animalNameListNW,
-    'SE': animalNameListSE,
-    'SW': animalNameListSW,
-  }
-  
+    [neLoc]: animalNameListNE,
+    [nwLoc]: animalNameListNW,
+    [seLoc]: animalNameListSE,
+    [swLoc]: animalNameListSW,
+  };
   return animalNameListOBJ;
 };
 
@@ -62,12 +66,13 @@ const getAnimalsNamesSorted = (animal, animalSex) => {
 
   // console.log('PARAMENTRO ANIMAL SEX', animalSex);
   if (animalSex) {
-    animalNames = residents.filter((resident) => resident.sex === animalSex).map((residentName) => residentName.name);
+    animalNames = residents.filter((resident) => resident.sex === animalSex)
+      .map((residentName) => residentName.name);
     // console.log('RETORNO DENTRO DO IF', animalNames)
   }
-  console.log('NOMES FILTRADOS POR SEXO', animalNames)
+  console.log('NOMES FILTRADOS POR SEXO', animalNames);
   const animalNamesSorted = animalNames.sort();
-  const objNames = {[animal]: animalNamesSorted};
+  const objNames = { [animal]: animalNamesSorted };
 
   return objNames;
 
@@ -75,25 +80,24 @@ const getAnimalsNamesSorted = (animal, animalSex) => {
 };
 
 const getListWithNamesSorted = (param1, paramSex) => {
-  // console.log('PARAM SEX PRIMEIRA FUNÇÃO', paramSex)
-  const sexOption = paramSex;
-  console.log('SEX DENTRO DA CONST FUNÇÃO 2', sexOption)
-  const allLocations =  getAllLocations();
+  const allLocations = getAllLocations();
   const { NE, NW, SE, SW } = allLocations;
   const animalNameListNE = NE.map((animal) => getAnimalsNamesSorted(animal, paramSex));
   const animalNameListNW = NW.map((animal) => getAnimalsNamesSorted(animal, paramSex));
   const animalNameListSE = SE.map((animal) => getAnimalsNamesSorted(animal, paramSex));
   const animalNameListSW = SW.map((animal) => getAnimalsNamesSorted(animal, paramSex));
+  const neLoc = 'NE';
+  const nwLoc = 'NW';
+  const seLoc = 'SE';
+  const swLoc = 'SW';
   const animalNameListOBJ = {
-    'NE': animalNameListNE,
-    'NW': animalNameListNW,
-    'SE': animalNameListSE,
-    'SW': animalNameListSW,
-  }
-  
+    [neLoc]: animalNameListNE,
+    [nwLoc]: animalNameListNW,
+    [seLoc]: animalNameListSE,
+    [swLoc]: animalNameListSW,
+  };
   return animalNameListOBJ;
 };
-
 
 // console.log(sortNames());
 // console.log(getListWithNames(getAllLocations()));
@@ -105,11 +109,11 @@ const getAnimalMap = (options) => {
   // console.log('PARAMETRO SEX DA FUNÇÃO PRINCIPAL', options.sex)
   if (options === undefined) return getAllLocations();
   if (!options.includeNames) return getAllLocations();
-  if (options.sex && options.sorted) return getListWithNamesSorted(null, options.sex)
+  if (options.sex && options.sorted) return getListWithNamesSorted(null, options.sex);
   if (options.sorted) return getListWithNamesSorted();
   if (options.sex) return getListWithNames(null, options.sex);
   if (options.includeNames) return getListWithNames();
-  
+
   // return listWithnames;
 };
 
@@ -117,13 +121,11 @@ const getAnimalMap = (options) => {
 
 module.exports = getAnimalMap;
 
-
-
-/// RETORNOS ///  
+/// RETORNOS ///
 
 // getLocation() => [ 'lions', 'giraffes' ]
 
-// getAllLocations() => 
+// getAllLocations() =>
 //  {
 //   NE: [ 'lions', 'giraffes' ],
 //   NW: [ 'tigers', 'bears', 'elephants' ],
@@ -133,7 +135,7 @@ module.exports = getAnimalMap;
 
 // getAnimalsNames() => { lions: [ 'Zena', 'Maxwell', 'Faustino', 'Dee' ] }
 
-// getListWithNames(getAllLocations()) => 
+// getListWithNames(getAllLocations()) =>
 // {
 //   NE: [ { lions: [Array] }, { giraffes: [Array] } ],
 //   NW: [ { tigers: [Array] }, { bears: [Array] }, { elephants: [Array] } ],
