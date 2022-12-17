389a0346acd74634d51a6cde646a975f007c1eb3
Felipe Mendes <felipemadruga@gmail.com>
2022-12-17 15:08:57 -0300

    Revisa e corrige requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,13 +4,13 @@ const { species } = data;
 
 const getAnimalsOlderThan = (animal, age) => {
   const animalFound = species.find((specie) => specie.name === animal);
-  // console.log(animalFound.residents);
   const agesMap = animalFound.residents.map((resident) => resident.age);
-  const minAge = agesMap.every((animalAge) => animalAge > age);
+  const minAge = agesMap.every((animalAge) => animalAge >= age);
 
   return minAge;
 };
 
 // console.log(getAnimalsOlderThan('penguins', 10));
+// console.log(getAnimalsOlderThan('otters', 8));
 
 module.exports = getAnimalsOlderThan;
